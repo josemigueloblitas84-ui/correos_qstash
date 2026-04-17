234ae97922668a6b9a9e31ec5e45da1fa44431a5
SirIsaac7 <isaacat7@gmail.com>
2026-04-17 14:28:11 -0400

    Se implemento larecipe de manera inicial

--- resources/views/correo.blade.php
+++
@@ -1,4 +0,0 @@
-<h2>Nuevo mensaje</h2>
-<p><strong>Nombre:</strong> {{ $data['nombre'] }}</p>
-<p><strong>Email:</strong> {{ $data['email'] }}</p>
-<p><strong>Mensaje:</strong> {{ $data['mensaje'] }}</p>
--- routes/api.php
+++ routes/api.php
@@ -21,23 +21,6 @@ use App\Http\Controllers\Api\UserController;
 |
 */
 
-/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});*/
-
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
-
-Route::middleware('jwt')->group(function(){
-    Route::get('/me', [AuthController::class, 'me']);
-    Route::post('/refresh', [AuthController::class, 'refresh']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-
-    Route::apiResource('users', UserController::class);
-    Route::apiResource('roles', RolController::class);
-    Route::apiResource('permisos', PermisoController::class);
-
-    Route::put('users/{user}/permisosespeciales', [UserController::class, 'updatePermisosEspeciales']);
 });
-
-Route::post('/qstash/enviar-email', [FormularioController::class, 'enviarEmail'])->middleware('qstash');
